df34a16d0f9f4411d9e8ec6473ccbf283aaa6207
unknown <falkunboy15@gmail.com>
2022-07-16 16:34:17 +0700

    web file

---
+++ Huy Khánh_I.R.E Gaming-20220513T024929Z-001/Huy Khánh_I.R.E Gaming/app.js
@@ -0,0 +1,15 @@
+// alert("Wecome to I.R.E")
+// const formElement = document.getElementById("info");
+
+// formElement.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     const data = {
+//         firstName : formElement.firstName.value,
+//         lastName : formElement.lastName.value,
+//         gender : formElement.gender.value,
+    
+       
+//     }
+//     console.log("data: ", data);})
+
+    
